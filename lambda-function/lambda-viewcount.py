b055225b61d2348136adb767ba778fcf43accb17
sp-howard <stevenphoward@gmail.com>
2023-06-26 15:38:39 -0700

    Modified Lambda to work with Terraform DynamoDB

---
+++ lambda-function/lambda-viewcount.py
@@ -0,0 +1,45 @@
+import boto3
+import os
+import json
+
+def lambda_handler(event: any, context: any):
+
+    # Create a DynamoDB client
+    dynamodb = boto3.resource("dynamodb")
+    # table_name = os.environ["TABLE_NAME"]
+    # table = dynamodb.Table(table_name)
+    table = dynamodb.Table('viewcount-table')
+    print('hello')
+
+    # Does 'viewed' table item exist? If not, initialize with viewcount value of 0
+    def get_key():
+        key = table.get_item(Key={'viewed': 'True'})
+        if 'Item' in key:
+            return key['Item']
+        else:
+            table.put_item(Item={'viewed': 'True', "viewcount": 0})
+            return table.get_item(Key={'viewed': 'True'})['Item']
+    
+    key = get_key()
+    print(key)
+    # Get current viewcount value
+    viewcount = key['viewcount']
+
+    # Increment viewcount by 1
+    updated_viewcount = viewcount + 1
+
+    # Add new viewcount value to table
+    table.put_item(Item={'viewed': 'True', "viewcount": updated_viewcount})
+
+    # Return API response in JSON format
+    return {
+        'statusCode': 200,
+        'headers': {
+            'Content-Type': 'application/json',
+        },
+        'body': {
+            'viewcount': updated_viewcount
+        }
+    }
+
+lambda_handler('','')
